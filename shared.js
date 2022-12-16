e6fc67717398064b1f5a521b50c619c17913954c
Robin Punnoose <robin.punnoose@protonmail.com>
2022-12-16 08:59:46 -0600

    added animations to navbar, packages, and customers page

--- shared.js
+++ shared.js
@@ -4,18 +4,19 @@ const selectPlanButton = document.querySelectorAll('.plan button');
 const negative = document.querySelector('.modal__action--negative');
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
+// const ctaButton = document.querySelector('.main-nav__item--cta');
 
 for (let i=0; i< selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', function() {
-        // modal.style.display = "block";
-        // backdrop.style.display = "block";
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(() => {
+            backdrop.classList.add('open');
+        },10)
     })
 }
 
 backdrop.addEventListener('click', function() {
-    // mobileNav.style.display= 'none';
     mobileNav.classList.remove('open');
     closeModal();
 })
@@ -25,17 +26,32 @@ if (negative) {
 }
 
 function closeModal() {
-    // modal.style.display = "none";
-    // backdrop.style.display = "none";
     if (modal) {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(() => {
+        backdrop.style.display = 'none';
+    },500)
+
 }
 
 toggleButton.addEventListener('click', function() {
-    // mobileNav.style.display = "block";
-    // backdrop.style.display = "block";
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+        backdrop.classList.add('open');
+    },10)
 })
+
+// ctaButton.addEventListener('animationstart', (event) => {
+//     console.log('animation started', event)
+// })
+
+// ctaButton.addEventListener('animationend', (event) => {
+//     console.log('animation ended', event)
+// })
+
+// ctaButton.addEventListener('animationiteration', (event) => {
+//     console.log('animation iteration', event)
+// })
